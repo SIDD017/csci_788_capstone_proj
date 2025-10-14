9816d48941c237c4c8c7fcd1f8262296a81e21a3
wrongu <richard.d.lange.13@dartmouth.edu>
2025-10-14 09:45:39 -0400

    test symmetric diff and use torch.testing module

--- tests/test_affine.py
+++ tests/test_affine.py
@@ -2,8 +2,15 @@ import unittest
 
 import numpy as np
 import torch
+import torch.testing as tt
 
-from affine import operator_norm, frobenius_norm, matrix_exp
+from affine import (
+    operator_norm,
+    frobenius_norm,
+    matrix_exp,
+    operator_diff,
+    frobenius_diff,
+)
 
 
 class TestLogParameterization(unittest.TestCase):
@@ -11,34 +18,39 @@ class TestLogParameterization(unittest.TestCase):
         log_A = torch.zeros(3, 4)
         A = matrix_exp(log_A)
         assert A.shape == log_A.shape
-        assert torch.allclose(A, torch.eye(2).repeat(3, 1, 1).view(3, 4))
+        tt.assert_close(A, torch.eye(2).repeat(3, 1, 1).view(3, 4))
 
     def test_log_2x2(self):
         log_A = torch.zeros(2, 2)
         A = matrix_exp(log_A)
         assert A.shape == log_A.shape
-        assert torch.allclose(A, torch.eye(2))
+        tt.assert_close(A, torch.eye(2))
 
     def test_log_Bx2x2(self):
         log_A = torch.zeros(3, 2, 2)
         A = matrix_exp(log_A)
         assert A.shape == log_A.shape
-        assert torch.allclose(A, torch.eye(2).repeat(3, 1, 1))
+        tt.assert_close(A, torch.eye(2).repeat(3, 1, 1))
 
     def test_log_Bx3x3(self):
         log_A = torch.zeros(5, 3, 3)
         A = matrix_exp(log_A)
         assert A.shape == log_A.shape
-        assert torch.allclose(A, torch.eye(3).repeat(5, 1, 1))
+        tt.assert_close(A, torch.eye(3).repeat(5, 1, 1))
 
 
 class TestOpNorm(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Seed chosen by keyboard-mashing
+        torch.manual_seed(1189357)
+
     def test_operator_norm_2x2(self):
         # Should work on a (2, 2) matrix
         A = torch.tensor([[3.0, 0.0], [0.0, 4.0]])
         norm = operator_norm(A)
         assert norm.shape == ()
-        assert torch.isclose(norm, torch.tensor(4.0))
+        tt.assert_close(norm, torch.tensor(4.0))
 
     def test_operator_norm_Bx2x2(self):
         # Should work on a batch of matrices
@@ -46,14 +58,14 @@ class TestOpNorm(unittest.TestCase):
         A_batch = torch.stack([A, 2 * A, 3 * A])  # Shape (3, 2, 2)
         norm_batch = operator_norm(A_batch)
         assert norm_batch.shape == (3,)
-        assert torch.allclose(norm_batch, torch.tensor([4.0, 8.0, 12.0]))
+        tt.assert_close(norm_batch, torch.tensor([4.0, 8.0, 12.0]))
 
     def test_operator_norm_3x3(self):
         # Should in principle work on a 3x3 matrix even though we don't use that
         A3 = torch.eye(3)
         norm3 = operator_norm(A3)
         assert norm3.shape == ()
-        assert torch.isclose(norm3, torch.tensor(1.0))
+        tt.assert_close(norm3, torch.tensor(1.0))
 
     def test_operator_norm_flat(self):
         # Should work on flattened square matrices
@@ -62,7 +74,7 @@ class TestOpNorm(unittest.TestCase):
         A_flat = A_batch.view(3, 4)  # Shape (3, 4)
         norm_flat = operator_norm(A_flat)
         assert norm_flat.shape == (3,)
-        assert torch.allclose(norm_flat, torch.tensor([4.0, 8.0, 12.0]))
+        tt.assert_close(norm_flat, torch.tensor([4.0, 8.0, 12.0]))
 
     def test_operator_norm_is_rotation_invariant(self):
         # The operator norm should be invariant under rotations
@@ -80,8 +92,25 @@ class TestOpNorm(unittest.TestCase):
 
         assert torch.isclose(norm_A, norm_A_rotated)
 
+    def test_operator_diff_is_symmetric(self):
+        # Run a batch of 100 randomized tests at once
+        A1 = torch.tensor([[3.0, 0.0], [0.0, 4.0]]).repeat(100, 1, 1) + torch.randn(
+            100, 2, 2
+        )
+        A2 = torch.tensor([[3.0, 0.0], [0.0, 4.0]]).repeat(100, 1, 1) + torch.randn(
+            100, 2, 2
+        )
+        diff_1_2 = operator_diff(A1, A2)
+        diff_2_1 = operator_diff(A2, A1)
+        tt.assert_close(diff_1_2, diff_2_1)
+
 
 class TestFrobNorm(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Seed chosen by keyboard-mashing
+        torch.manual_seed(17894165)
+
     def test_frobenius_norm_2x2(self):
         # Should work on a (2, 2) matrix
         A = torch.tensor([[3.0, 0.0], [0.0, 4.0]])
@@ -95,7 +124,7 @@ class TestFrobNorm(unittest.TestCase):
         A_batch = torch.stack([A, 2 * A, 3 * A])  # Shape (3, 2, 2)
         norm_batch = frobenius_norm(A_batch)
         assert norm_batch.shape == (3,)
-        assert torch.allclose(norm_batch, torch.tensor([5.0, 10.0, 15.0]))
+        tt.assert_close(norm_batch, torch.tensor([5.0, 10.0, 15.0]))
 
     def test_frobenius_norm_3x3(self):
         # Should in principle work on a 3x3 matrix even though we don't use that
@@ -111,7 +140,7 @@ class TestFrobNorm(unittest.TestCase):
         A_flat = A_batch.view(3, 4)  # Shape (3, 4)
         norm_flat = frobenius_norm(A_flat)
         assert norm_flat.shape == (3,)
-        assert torch.allclose(norm_flat, torch.tensor([5.0, 10.0, 15.0]))
+        tt.assert_close(norm_flat, torch.tensor([5.0, 10.0, 15.0]))
 
     def test_frobenius_norm_is_rotation_invariant(self):
         # The frobenius norm is expected to be invariant under rotations
@@ -127,4 +156,16 @@ class TestFrobNorm(unittest.TestCase):
         A_rotated = R @ A @ R.T
         norm_A_rotated = frobenius_norm(A_rotated)
 
-        assert torch.isclose(norm_A, norm_A_rotated)
+        tt.assert_close(norm_A, norm_A_rotated)
+
+    def test_frobenius_diff_is_symmetric(self):
+        # Run a batch of 100 randomized tests at once
+        A1 = torch.tensor([[3.0, 0.0], [0.0, 4.0]]).repeat(100, 1, 1) + torch.randn(
+            100, 2, 2
+        )
+        A2 = torch.tensor([[3.0, 0.0], [0.0, 4.0]]).repeat(100, 1, 1) + torch.randn(
+            100, 2, 2
+        )
+        diff_1_2 = frobenius_diff(A1, A2)
+        diff_2_1 = frobenius_diff(A2, A1)
+        tt.assert_close(diff_1_2, diff_2_1)
